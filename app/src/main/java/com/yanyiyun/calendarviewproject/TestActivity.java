fe253c01190d0bde23a338823a1735fdc923ee76
严益云 <1206995290@qq.com>
2020-04-24 12:46:00 +0800

    first commit

---
+++ app/src/main/java/com/yanyiyun/calendarviewproject/TestActivity.java
@@ -0,0 +1,45 @@
+package com.yanyiyun.calendarviewproject;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import com.yanyiyun.calendarviewproject.base.activity.BaseActivity;
+
+/**
+ * 测试界面
+ * Created by yanyiyun on 2018/8/7.
+ */
+
+public class TestActivity extends BaseActivity {
+
+    private FrameLayout mLayout;
+
+    public static void show(Context context) {
+        context.startActivity(new Intent(context, TestActivity.class));
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_test;
+    }
+
+    @Override
+    protected void initView() {
+        setStatusBarDarkMode();
+        mLayout = (FrameLayout) findViewById(R.id.customLayout);
+    }
+
+    @Override
+    protected void initData() {
+        mLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                View view = findViewById(R.id.emptyView);
+                view.requestLayout();
+
+            }
+        });
+    }
+}
---
+++ calendarview/src/androidTest/java/com/yanyiyun/calendarview/SolarTermUtilTest.java
@@ -0,0 +1,15 @@
+package com.yanyiyun.calendarview;
+
+import org.junit.Test;
+
+/**
+ * 24节气单元测试
+ * Created by yanyiyun on 2018/2/9.
+ */
+public class SolarTermUtilTest {
+    @Test
+    public void getSolarTerms() throws Exception {
+        SolarTermUtil.getSolarTerms(2017);
+    }
+
+}
